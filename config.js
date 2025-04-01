d88a09eb5fc1e1a8f15d4b048e540fa52dc62d86
Daily Best News App <146649776+MustakimRidoyMR@users.noreply.github.com>
2025-04-01 16:33:05 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // এই ফাইলটি .gitignore এ যোগ করুন
 export const config = {
-    SHEET_ID: 'আপনার_Google_Sheet_ID',
+    SHEET_ID: '1ro17AE63-xBAVk2ID9ZbbmKIAPAxrt5TPbDOJb70u_I',
     API_KEY: 'আপনার_API_Key'
 };
