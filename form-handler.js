23673f85d1198a3d458d074e807cc98807d3b2b2
Daily Best News App <146649776+MustakimRidoyMR@users.noreply.github.com>
2025-04-01 16:21:06 +0600

    Update form-handler.js

--- form-handler.js
+++ form-handler.js
@@ -2,6 +2,6 @@ import { config } from './config.js';
 
 // Google Sheets API কনফিগারেশন
 const SHEET_ID = config.SHEET_ID;
-const API_KEY = config.API_KEY;
+const K = config.API_KEY;
 
 // বাকি কোড একই থাকবে
